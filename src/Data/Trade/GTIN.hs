e4569e83b76b466fdd1871bc7b4ad3e935937e56
Willem Van Onsem <vanonsem.willem@gmail.com>
2023-06-23 22:32:28 +0200

    start of gtin project

--- src/Data/Trade/GTIN.hs
+++ src/Data/Trade/GTIN.hs
@@ -1,5 +1,15 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+
 module Data.Trade.GTIN (GTIN (GTIN)) where
 
-import Data.Word(Word64)
+import Data.Word (Word64)
+import GHC.Num.Natural (Natural)
 
-newtype GTIN = GTIN Word64 deriving (Eq, Ord, Read, Show)
+newtype GTIN (n :: Natural) = GTIN Word64 deriving (Eq, Ord, Read, Show)
+
+type GTIN14 = GTIN 14
+
+type EAN = GTIN14
+
+type GTIN8 = GTIN 8
