8777ac592b00447bb5d52b228b0f0e3058f19326
Willem Van Onsem <vanonsem.willem@gmail.com>
2023-06-24 15:36:17 +0200

    make hashable

--- src/Data/Trade/GTIN.hs
+++ src/Data/Trade/GTIN.hs
@@ -1,15 +1,45 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module Data.Trade.GTIN (GTIN (GTIN)) where
+module Data.Trade.GTIN (GTIN (GTIN), GTIN14, GTIN13, GTIN12, GTIN8, EANUCC14, SCC14, EAN, EANUCC13, ISBN, ISBN13, EANUCC8) where
 
+import Data.Data (Data)
+import Data.Hashable (Hashable)
+import Data.Typeable (Typeable)
 import Data.Word (Word64)
+import GHC.Generics (Generic)
 import GHC.Num.Natural (Natural)
+import GHC.TypeNats (KnownNat, natVal)
 
-newtype GTIN (n :: Natural) = GTIN Word64 deriving (Eq, Ord, Read, Show)
+newtype GTIN (n :: Natural) = GTIN Word64 deriving (Data, Eq, Generic, Ord, Read, Show, Typeable)
+
+instance Hashable (GTIN n)
+
+instance KnownNat n => Bounded (GTIN (n :: Natural)) where
+  minBound = GTIN 0
+  maxBound = GTIN (10 ^ natVal (undefined :: GTIN n) - 1)
 
 type GTIN14 = GTIN 14
 
-type EAN = GTIN14
+type GTIN13 = GTIN 13
+
+type GTIN12 = GTIN 12
 
 type GTIN8 = GTIN 8
+
+type EANUCC14 = GTIN14
+
+type SCC14 = GTIN14
+
+type EAN = GTIN13
+
+type EANUCC13 = GTIN13
+
+type ISBN = GTIN13
+
+type ISBN13 = GTIN13
+
+type EANUCC8 = GTIN8
